2f6ec427341ae0b73e2bf6a49e0079788b8ee946
anicho <Kuzmina.vika2012@yandex.ru>
2023-09-08 04:12:59 +0300

    08.09

---
+++ src/OOP/seminarOOP/seminar4/task5/Gen.java
@@ -0,0 +1,9 @@
+package OOP.seminarOOP.seminar4.task5;
+
+public class Gen<T> {
+    T ob;
+
+    Gen (T o) {
+        this.ob = o;
+    }
+}
---
+++ src/OOP/seminarOOP/seminar4/task5/Main.java
@@ -0,0 +1,17 @@
+package OOP.seminarOOP.seminar4.task5;
+
+class A {
+    // тело класса
+}
+
+class B extends A {
+    // тело класса
+}
+
+class C extends A {
+    // тело класса
+}
+
+class D {
+    // тело класса
+}
